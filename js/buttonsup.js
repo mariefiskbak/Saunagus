58e24ab4e9251f08b15eab4e4e1e47600ecbcee7
Marie Fiskbæk <marie.fiskbak@gmail.com>
2022-03-17 12:52:43 +0100

    No commitment issues

---
+++ js/buttonsup.js
@@ -0,0 +1,15 @@
+
+    var el_up = document.getElementById("jegertrykketned");
+    var el_down = document.getElementById("GFG_DOWN");
+    var str = "Click on button to change the background color";
+
+    el_up.innerHTML = str;
+
+    function changeColor(color) {
+    document.body.style.background = color;
+}
+
+    function gfg_Run() {
+    changeColor('yellow');
+    el_down.innerHTML = "Background Color changed";
+}
